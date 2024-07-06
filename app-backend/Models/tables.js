2b7ca8a81ee584c4d93dd3832d9cf3af3c32a7fd
Camille <c.fbball@hotmail.co.uk>
2024-07-06 17:33:38 +0100

    refactor: removed the mongodb code, added some SQL boilerplate for later, added some catch-all routes on the base of the API to handle unwanted requests

---
+++ app-backend/Models/tables.js
@@ -0,0 +1,17 @@
+
+/*
+- mineSweeper data
+    - id
+        - default/ primary
+    - playerId
+        - string
+    - playerName
+        - string
+        - min 3
+        - max 20
+    - score
+        - int
+    - time
+        - DNF
+        - stringified time for conversion later
+*/
--- app-backend/app.js
+++ app-backend/app.js
@@ -3,6 +3,7 @@ const cors = require('cors') //needed for connection between backend and fronten
 const app = express()
 
 const upload = require('./routes/upload')
+const minesweeper = require('./routes/mineSweeper')
 
 module.exports= app
 
@@ -10,4 +11,34 @@ app.use(cors());
 app.use(express.json({ limit: '2mb' }));
 app.use(express.urlencoded({ limit: '2mb', extended: true }));
 
-app.use("/imageupload", upload) ;
+app.use("/imageupload", upload);
+app.use("/gn-minesweeper", minesweeper);
+
+
+
+// CATCH ALLS
+
+app.post("/*", (req,res) => {
+    res.status(502).send({
+        "error": "request not allowed".toUpperCase()
+    }) // return unauthorized response or request not available response
+})
+
+app.put("/*", (req,res) => {
+    res.status(502).send({
+        "error": "request not allowed".toUpperCase()
+    }) 
+})
+
+app.patch("/*", (req,res) => {
+    res.status(502).send({
+        "error": "request not allowed".toUpperCase()
+    }) 
+})
+
+app.delete("/*", (req,res) => {
+    res.status(502).send({
+        "error": "request not allowed".toUpperCase()
+    }) 
+})
+
